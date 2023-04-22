c7574d214fd5f04d9e89ab9ecfe95d2a11989b30
Rajdip Das <dasrajdip78754282@gmail.com>
2023-04-22 17:39:46 +0530

    Initial commit

--- SQL/2.Advanced-SQL/France_query.sql
+++ SQL/2.Advanced-SQL/France_query.sql
@@ -1,2 +1,13 @@
 /* HOW MANY TOWNS ARE THERE IN FRANCE? */
 -- SELECT COUNT(code) FROM towns;
+
+/* COUNT THE NUMBER OF TOWNS PER REGION */
+-- SELECT DISTINCT a.id, a."name", COUNT(c.id)
+-- OVER (
+--     PARTITION BY a.id
+--     ORDER BY a."name"
+-- ) AS "# of towns"
+-- FROM regions AS a
+-- JOIN departments AS b ON a.code = b.region 
+-- JOIN towns AS c ON b.code = c.department
+-- ORDER BY a.id;
